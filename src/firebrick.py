dae87bc4eaecc4f834b6c19678373fd9538bba13
Philip Linden <lindenphilipj@gmail.com>
2021-05-22 14:02:13 -0700

    use sparse matrices instead

--- src/firebrick.py
+++ src/firebrick.py
@@ -5,8 +5,7 @@ from utils import (
     get_boundary_nodes,
     get_source_nodes,
 )
-from steadystate import steady_state
-from transient import transient
+from nodetools import NodeMap
 
 ''' ASSUMPTIONS
 1. convection heat transfer is linear
@@ -18,6 +17,7 @@ from transient import transient
 
 
 def main():
+    import time
     nodes = pd.read_csv('nodes.csv')
     connections = pd.read_csv('connections.csv')
     connections['C'] = connections['R'].apply(conductance)
@@ -25,9 +25,14 @@ def main():
     print(connections)
     print('boundary nodes: %s' % get_boundary_nodes(nodes)['name'].to_list())
     print('source nodes: %s' % get_source_nodes(nodes)['name'].to_list())
-    steady_state(nodes, connections)
-    transient(nodes, connections, 1000, 0.1)
-
+    tic = time.perf_counter()
+    nodemap = NodeMap(nodes, connections)
+    print(f'Conductance coupling matrix, K\n{nodemap.K}')
+    print(f'Heat load and boundary condition vector, Q\n{nodemap.Q}')
+    temps = nodemap.steady_state()
+    toc = time.perf_counter()
+    print(pd.Series(temps, index=nodemap.Kmap.keys(), name='Steady State Temps'))
+    print(f'runtime: {toc - tic:0.4f} seconds')
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
-from scipy.sparse import coo_matrix
+from scipy import sparse
 import pandas as pd
-
+import numpy as np
 
 def pairs2square(connections, var, fillna=0):
     pairs = connections[['node1', 'node2', var]]
@@ -13,8 +13,9 @@ def pairs2square(connections, var, fillna=0):
     return square_matrix
 
 
-def to_sparse_matrix(connections, var):
-    ''' turn a connections dataframe into a sparse matrix in coordinate format
+def to_sparse_matrix(connections, var=None):
+    ''' turn a connections dataframe into a square sparse matrix in
+        coordinate format [(row,col) data]
     'node1' --> row coordinates
     'node2' --> col coordinates
       var   --> data at coordinates
@@ -22,14 +23,43 @@ def to_sparse_matrix(connections, var):
     index_keys = pd.unique(connections[['node1', 'node2']].values.ravel('K'))
     index_vals = range(0,len(index_keys))
     index_map = {k: v for k, v in zip(index_keys, index_vals)}
-
+    num_nodes = len(index_map.keys())
     connection_pairs = connections[['node1', 'node2']].applymap(
             lambda x: index_map[x], na_action='ignore').values
-    data = connections[var].values
 
-    sparse_matrix = coo_matrix(
-        (data, (connection_pairs[:, 0], connection_pairs[:, 1])))
+    # flip rows and columns and plop the repeat same data
+    rows = np.hstack((connection_pairs[:, 0], connection_pairs[:, 1]))
+    cols = np.hstack((connection_pairs[:, 1], connection_pairs[:, 0]))
+    if var:
+        data = np.hstack((connections[var].values, connections[var].values))
+    else:
+        data = np.ones(rows.shape)
+
+    sparse_matrix = sparse.coo_matrix(
+        (data, (rows, cols)), shape=(num_nodes, num_nodes))
     return sparse_matrix, index_map
+
+
+def coupling_from_conductances(connections):
+    conductance_spmatrix, index_map = to_sparse_matrix(connections, 'C')
+    diagonals = np.diagflat(-np.sum(conductance_spmatrix, axis=1))
+    K_spmatrix = conductance_spmatrix + diagonals
+    return K_spmatrix, index_map
+
+
+def delete_indices_symmetric(spmatrix, indices_to_drop):
+    ''' delete rows and columns at indices from sparse matrix '''
+    rows_to_keep = list(set(range(spmatrix.shape[0]))-set(indices_to_drop))
+    spmatrix = spmatrix[rows_to_keep,:]
+    cols_to_keep = list(set(range(spmatrix.shape[1]))-set(indices_to_drop))
+    spmatrix = spmatrix[:, cols_to_keep]
+    return spmatrix
+
+
+def delete_indices_vector(vector, indices_to_drop):
+    ''' delete rows and columns at indices from sparse matrix '''
+    to_keep = list(set(range(vector.shape[0]))-set(indices_to_drop))
+    return vector[to_keep]
 
 
 def get_boundary_nodes(nodes):
@@ -38,7 +68,7 @@ def get_boundary_nodes(nodes):
 
 
 def get_source_nodes(nodes):
-    source_nodes = nodes[nodes['dissipated_heat'] > 0]
+    source_nodes = nodes[nodes['external_load'] > 0]
     return source_nodes
 
 
